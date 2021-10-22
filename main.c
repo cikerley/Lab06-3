0a346d7dc831abb94d502ec5843e726f1ca2ec6a
cikerley <cikerley@lipscomb.edu>
2021-10-22 08:57:57 -0500

    init

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * Lab06-2.c
+ *
+ * Created: 10/22/2021 8:35:50 AM
+ * Author : cikerley
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
